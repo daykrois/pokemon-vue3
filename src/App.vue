6297718235194bc6dbe374ffdd4bb1a0db6dd5d1
daykrois <saiyanqlwy@foxmail.com>
2024-03-11 21:46:53 +0800

    基础布局

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,54 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, reactive } from 'vue';
+
+
+onMounted(() => {
+  getPokemonList();
+})
+
+function getPokemonList() {
+  fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+    .then(response => response.json())
+    .then(data => {
+      data.results.forEach(pokemon => {
+        pokemonList.push(pokemon)
+      });
+    })
+}
+
+function getPokemonName(){
+
+}
+
+const pokemonList = reactive([])
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row justify="center">
+          <v-col v-for="pokemon in pokemonList" cols="3">
+            <v-card variant="tonal" >
+              <v-card-item>
+                <v-card-title>{{ pokemon.name }}</v-card-title>
+              </v-card-item>
+            </v-card>
+          </v-col>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+        </v-row>
+      </v-container>
+      <!-- <v-pagination v-model="page" :length="5" rounded="circle"></v-pagination> -->
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.v-app {
+  background-color: rgb(178, 180, 184);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
